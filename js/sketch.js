e1030158f80d3262d2bdce6d9c57b9251c2a3ce8
Philo van Kemenade <philovankemenade@gmail.com>
2017-05-26 19:00:05 +0200

    draw petals using splineloop

--- js/sketch.js
+++ js/sketch.js
@@ -8,6 +8,7 @@ var petals_amount = 8;
 var petals_radius = 150;
 var petals_size = 150;
 var petals_color = [47, 32, 51];
+var petals_nPoints = 5;
 
 var stamens_amount = 5;
 var stamens_radius = 30;
@@ -35,8 +36,8 @@ function setup() {
     // Create Layout GUI
     guiSepals  = createGui('Sepals');
     guiPetals  = createGui('Petals');
-    guiStamens = createGui('Stamens');
-    guiCarpel  = createGui('Carpel');
+    // guiStamens = createGui('Stamens');
+    // guiCarpel  = createGui('Carpel');
 
     guiSepals.addGlobals(
         'sepals_amount',
@@ -49,19 +50,20 @@ function setup() {
         'petals_radius',
         'petals_size',
         'petals_color',
+        'petals_nPoints',
     );
-    guiStamens.addGlobals(
-        'stamens_amount',
-        'stamens_radius',
-        'stamens_size',
-        'stamens_color',
-    );
-    guiCarpel.addGlobals(
-        'carpel_amount',
-        'carpel_radius',
-        'carpel_size',
-        'carpel_color',
-    );
+    // guiStamens.addGlobals(
+    //     'stamens_amount',
+    //     'stamens_radius',
+    //     'stamens_size',
+    //     'stamens_color',
+    // );
+    // guiCarpel.addGlobals(
+    //     'carpel_amount',
+    //     'carpel_radius',
+    //     'carpel_size',
+    //     'carpel_color',
+    // );
 
 
     myFlower = new Flower();
@@ -86,7 +88,7 @@ function Flower() {
 
         for (var i = 0; i < petals_amount; i++) {
             var pos = getPosOnCircle(this.position, petals_radius, petals_amount, i);
-            draw_petal(pos, petals_size, petals_color);
+            draw_petal(pos, petals_size, petals_nPoints, petals_color);
         }
 
         for (var i = 0; i < stamens_amount; i++) {
@@ -120,9 +122,27 @@ function draw_sepal(position, size, color) {
     ellipse(position.x, position.y, size, size);
 }
 
-function draw_petal(position, size, color) {
-    fill(color);
-    ellipse(position.x, position.y, size, size);
+function draw_petal(position, size, nPoints, color) {
+    console.log("draw_petal");
+    
+    // fill(255,0,0);
+    // ellipse(position.x, position.y, size, size);
+
+    // noFill();
+    fill(color[0],color[1],color[2], 180);
+
+    var positions = _.range(nPoints).map(function(currentValue, index, array) {
+        return getPosOnCircle(position, size, nPoints, index);
+    });
+
+    
+    drawSplineLoop(positions);
+
+    // positions.map(function(currentValue, index, array) {
+    //     ellipse(currentValue.x, currentValue.y, 50, 50);
+    // });
+
+
 }
 
 function draw_stamen(position, size, color) {
@@ -135,6 +155,17 @@ function draw() {
     background(0);
 
     myFlower.draw();
+}
+
+function drawSplineLoop(points) {
+    beginShape();
+        for (var i = 0; i < points.length; i++) {
+            curveVertex(points[i].x, points[i].y);
+        }   
+        curveVertex(points[0].x, points[0].y);
+        curveVertex(points[1].x, points[1].y);
+        curveVertex(points[2].x, points[2].y);
+    endShape();
 }
 
 function windowResized() {
